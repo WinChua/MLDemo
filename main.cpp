3e28593817c48aa1a2472da33fa3ad686f20bd2a
WinChua <winchua@foxmail.com>
2018-02-28 16:28:58 +0800

    add Makefile

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@
 #include "DecisionTree.hpp"
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    File train("data/libsvm.txt");
-    File test("data/libsvm_test.txt");
+    if(argc < 3)
+    {
+        std::cout << "you should specify the train and test file like:" << std::endl;
+        std::cout << "MLDemo train.file test.file" << std::endl;
+        return 1;
+    }
+    std::string trainfile(argv[1]), testfile(argv[2]);
+    File train(trainfile);
+    File test(testfile);
     //train.show_features();
     TreeNode root(train);
     train.show_statistic();
     test.show_statistic();
     root.test(test);
-    system("pause");
     return 0;
 }
